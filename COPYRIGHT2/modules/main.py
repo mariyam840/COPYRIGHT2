024e3542f6139e2f75593a52bdd0ed794e8aa3d8
mariyam840 <159921630+mariyam840@users.noreply.github.com>
2024-03-22 22:40:08 +0530

    Update main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -40,7 +40,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/7f8ebddf56559ac69d31b.jpg",
+        photo="https://graph.org/file/f4270ab738646e60864a4.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
